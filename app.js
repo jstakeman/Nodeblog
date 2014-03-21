44f9f221ecc32d13128e413342b46b6b593aec14
Jackson Stakeman <jstakeman@gmail.com>
2014-03-20 20:42:39 -0600

    Adds procfile and configures weak authentication

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var post = require('./routes/post');
+var signin = require('./routes/login.js');
 var http = require('http');
 var path = require('path');
 var app = express();
@@ -28,6 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('1234'));
+app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,13 +39,24 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
 app.get('/', routes.index(db));
-app.get('/newpost', post.new);
-app.post('/createpost', post.create(db));
-app.del('/deletepost/:_id', post.delete(db));
+app.get('/newpost', restrict, post.new);
+app.post('/createpost', restrict, post.create(db));
+app.del('/deletepost/:_id', restrict, post.delete(db));
 app.get('/post/:slug', post.show(db));
-app.get('/post/edit/:_id', post.edit(db));
-app.put('/post/update/:_id', post.update(db));
+app.get('/post/edit/:_id', restrict, post.edit(db));
+app.put('/post/update/:_id', restrict, post.update(db));
+app.get('/login', signin.login);
+app.post('/login', signin.check(env));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
